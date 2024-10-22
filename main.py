d42ca1c02bb06ec659c9ce1191010d8682434e9c
mtssoh <dominguezmatias420@gmail.com>
2024-10-22 13:25:22 -0300

    can move player now

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
+    clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+    
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        pygame.display.flip()
+       
+        for obj in updatable:
+            obj.update(dt)
 
-    
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        dt = clock.tick(60) / 1000
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
